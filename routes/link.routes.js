af06e5e50629e27c03cc42199c7c965211037e38
xirly <vlad.doronyuk2000@gmail.com>
2021-11-21 23:52:47 +0200

    update

--- routes/link.routes.js
+++ routes/link.routes.js
@@ -8,8 +8,7 @@ const router = Router();
 
 router.post("/generate", auth, async (req, res) => {
   try {
-    const baseUrl = process.env.HEROKU_APP_NAME || config.get("baseUrl");
-    console.log(process.env.HEROKU_APP_NAME);
+    const baseUrl = process.env.BASE_URL || config.get("baseUrl");
     const { from } = req.body;
 
     const code = nanoid();
